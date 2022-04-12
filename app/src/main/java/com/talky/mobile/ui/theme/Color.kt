32ecee0ff7f8d670a20e88267359ea7b7ad68b7f
Adrien-Ba <adrien.bassail@live.fr>
2022-04-12 08:40:21 +0200

    :lipstick: Add main colors

--- app/src/main/java/com/talky/mobile/ui/theme/Color.kt
+++ app/src/main/java/com/talky/mobile/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.talky.mobile.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+val VioletClair = Color(0xAB98F8)
+val VioletFonce = Color(0x756B9F)
+val BlancFonce = Color(0xEDF0ED)
+val BleuCyan = Color(0x5ACCE0)
