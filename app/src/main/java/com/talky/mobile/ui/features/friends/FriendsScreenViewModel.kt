588310999ebd0628e9ade977fd8d37d4f53c6b32
HenriBoulnois <72384047+HenriBoulnois@users.noreply.github.com>
2022-05-25 21:52:17 +0200

    User bar (#5)
     :lipstick: UserBar and FriendsScreen for a preview

---
+++ app/src/main/java/com/talky/mobile/ui/features/friends/FriendsScreenViewModel.kt
@@ -0,0 +1,10 @@
+package com.talky.mobile.ui.features.friends
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class FriendsScreenViewModel @Inject constructor() : ViewModel() {
+
+}
